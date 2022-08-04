e80e05a5b69d74cc6e9afed1f5781d2667bc68b5
HBS Development Team <dev.hbsolutions@gmail.com>
2022-08-04 20:13:02 +0700

    [MINOR] Add Identity abstract class.

---
+++ src/Exception/MisconfigurationException.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+namespace HBS\Auth\Exception;
+
+class MisconfigurationException extends \LogicException implements ExceptionInterface
+{
+}
---
+++ src/Model/Identity/AbstractIdentity.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace HBS\Auth\Model\Identity;
+
+use HBS\Auth\Exception\MisconfigurationException;
+
+class AbstractIdentity implements IdentityInterface
+{
+    /**
+     * @var string
+     */
+    protected const DOMAIN = null;
+
+    public function getDomain(): string
+    {
+        $domain = static::DOMAIN;
+
+        if (empty($domain) || !is_string($domain)) {
+            throw new MisconfigurationException("Identity Domain not defined");
+        }
+
+        return $domain;
+    }
+}
