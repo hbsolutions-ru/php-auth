72c6ae6be48fb754ea90a7b4ec6b5f6c64acef8e
HBS Development Team <dev.hbsolutions@gmail.com>
2022-08-15 22:03:18 +0700

    [MINOR] Refactoring of Immutable Objects.

--- src/Exception/AccessViolationException.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace HBS\Auth\Exception;
-
-class AccessViolationException extends \LogicException implements ExceptionInterface
-{
-}
--- src/Immutable/JwtSettings.php
+++ src/Immutable/JwtSettings.php
@@ -2,20 +2,18 @@
 
 namespace HBS\Auth\Immutable;
 
-use HBS\Auth\Exception\AccessViolationException;
+use HBS\Immutable\AbstractImmutable;
 
 /**
  * @property string $algorithm The signing algorithm
  * @property int $expiration JWT expiration time in seconds
  * @property string $secret The secret key
  */
-final class JwtSettings
+final class JwtSettings extends AbstractImmutable
 {
     private const PROPERTY_ALGORITHM = 'algorithm';
     private const PROPERTY_EXPIRATION = 'expiration';
     private const PROPERTY_SECRET = 'secret';
-
-    private $data;
 
     public function __construct(
         string $algorithm,
@@ -25,23 +23,7 @@ final class JwtSettings
         $this->data[self::PROPERTY_ALGORITHM] = $algorithm;
         $this->data[self::PROPERTY_EXPIRATION] = $expiration;
         $this->data[self::PROPERTY_SECRET] = $secret;
-    }
 
-    public function __get(string $name)
-    {
-        if (array_key_exists($name, $this->data)) {
-            return $this->data[$name] ?? null;
-        }
-        throw new AccessViolationException(sprintf("Property '%s' does not exist", $name));
-    }
-
-    public function __set(string $name, $value): void
-    {
-        throw new AccessViolationException("Object modification is forbidden");
-    }
-
-    public function __unset(string $name): void
-    {
-        throw new AccessViolationException("Object modification is forbidden");
+        parent::__construct();
     }
 }
