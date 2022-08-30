b10f4ba5531b60543029eac79d495c2232283ac1
HBS Development Team <dev.hbsolutions@gmail.com>
2022-08-30 14:42:35 +0700

    [BUGFIX] Implementation of 'getPayload' method for Credentials/Token.

--- src/Model/Credentials/Token.php
+++ src/Model/Credentials/Token.php
@@ -9,13 +9,24 @@ class Token implements TokenInterface
      */
     protected $token;
 
-    public function __construct(string $token)
+    /**
+     * @var array
+     */
+    protected $payload;
+
+    public function __construct(string $token, array $payload = [])
     {
         $this->token = $token;
+        $this->payload = $payload;
     }
 
     public function getToken(): string
     {
         return $this->token;
+    }
+
+    public function getPayload(): array
+    {
+        return $this->payload;
     }
 }
